d2bb2abad9318b4315fe84a1aec9c70283d65105
Denis Ciccale <dciccale@gmail.com>
2017-05-07 19:40:00 +0200

    feat(lib): version 3.0.0 allow multiple messages on OrderFulfillment

--- test/xml.js
+++ test/xml.js
@@ -15,7 +15,7 @@ describe('xml', () => {
                     StatusCode: 'Success',
                     MerchantIdentifier: '67890'
                 });
-                const xml = '<?xml version="1.0" encoding="iso-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion><MerchantIdentifier>67890</MerchantIdentifier></Header><MessageType>OrderAcknowledgement</MessageType><Message><MessageID>1</MessageID><OrderAcknowledgement><AmazonOrderID>123-4567890-1234567</AmazonOrderID><MerchantOrderID>12345</MerchantOrderID><StatusCode>Success</StatusCode></OrderAcknowledgement></Message></AmazonEnvelope>'
+                const xml = '<?xml version="1.0" encoding="ISO-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion><MerchantIdentifier>67890</MerchantIdentifier></Header><MessageType>OrderAcknowledgement</MessageType><Message><MessageID>1</MessageID><OrderAcknowledgement><AmazonOrderID>123-4567890-1234567</AmazonOrderID><MerchantOrderID>12345</MerchantOrderID><StatusCode>Success</StatusCode></OrderAcknowledgement></Message></AmazonEnvelope>'
 
                 expect(FeedContent).to.equal(xml);
             });
@@ -32,7 +32,7 @@ describe('xml', () => {
                         MerchantOrderItemID: '54321'
                     }
                 });
-                const xml = '<?xml version="1.0" encoding="iso-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion><MerchantIdentifier>67890</MerchantIdentifier></Header><MessageType>OrderAcknowledgement</MessageType><Message><MessageID>1</MessageID><OrderAcknowledgement><AmazonOrderID>123-4567890-1234567</AmazonOrderID><MerchantOrderID>12345</MerchantOrderID><StatusCode>Success</StatusCode><Item><AmazonOrderItemCode>12345</AmazonOrderItemCode><MerchantOrderItemID>54321</MerchantOrderItemID></Item></OrderAcknowledgement></Message></AmazonEnvelope>'
+                const xml = '<?xml version="1.0" encoding="ISO-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion><MerchantIdentifier>67890</MerchantIdentifier></Header><MessageType>OrderAcknowledgement</MessageType><Message><MessageID>1</MessageID><OrderAcknowledgement><AmazonOrderID>123-4567890-1234567</AmazonOrderID><MerchantOrderID>12345</MerchantOrderID><StatusCode>Success</StatusCode><Item><AmazonOrderItemCode>12345</AmazonOrderItemCode><MerchantOrderItemID>54321</MerchantOrderItemID></Item></OrderAcknowledgement></Message></AmazonEnvelope>'
 
                 expect(FeedContent).to.equal(xml);
             });
@@ -42,14 +42,18 @@ describe('xml', () => {
             it('should handle a single tracking number', () => {
                 const FeedContent = Feeds._POST_ORDER_FULFILLMENT_DATA_({
                     MessageID: 1,
-                    MerchantOrderID: 1234567,
-                    MerchantFulfillmentID: 1234567,
-                    FulfillmentDate: '2002-05-01T15:36:33-08:00',
-                    CarrierCode: 'UPS',
-                    ShippingMethod: 'Second Day',
-                    ShipperTrackingNumber: '1234567890'
+                    OrderFulfillment: {
+                      MerchantOrderID: 1234567,
+                      MerchantFulfillmentID: 1234567,
+                      FulfillmentDate: '2002-05-01T15:36:33-08:00',
+                      FulfillmentData: {
+                        CarrierCode: 'UPS',
+                        ShippingMethod: 'Second Day',
+                        ShipperTrackingNumber: '1234567890'
+                      }
+                    }
                 });
-                const xml = '<?xml version="1.0" encoding="iso-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>'
+                const xml = '<?xml version="1.0" encoding="ISO-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>'
 
                 expect(FeedContent).to.equal(xml);
             });
@@ -57,14 +61,18 @@ describe('xml', () => {
             it('should handle a single tracking number as an array', () => {
                 const FeedContent = Feeds._POST_ORDER_FULFILLMENT_DATA_({
                     MessageID: 1,
-                    MerchantOrderID: 1234567,
-                    MerchantFulfillmentID: 1234567,
-                    FulfillmentDate: '2002-05-01T15:36:33-08:00',
-                    CarrierCode: 'UPS',
-                    ShippingMethod: 'Second Day',
-                    ShipperTrackingNumber: ['1234567890']
+                    OrderFulfillment: {
+                      MerchantOrderID: 1234567,
+                      MerchantFulfillmentID: 1234567,
+                      FulfillmentDate: '2002-05-01T15:36:33-08:00',
+                      FulfillmentData: {
+                        CarrierCode: 'UPS',
+                        ShippingMethod: 'Second Day',
+                        ShipperTrackingNumber: ['1234567890']
+                      }
+                    }
                 });
-                const xml = '<?xml version="1.0" encoding="iso-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>'
+                const xml = '<?xml version="1.0" encoding="ISO-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>'
 
                 expect(FeedContent).to.equal(xml);
             });
@@ -72,19 +80,52 @@ describe('xml', () => {
             it('should handle multiple tracking numbers', () => {
                 const FeedContent = Feeds._POST_ORDER_FULFILLMENT_DATA_({
                     MessageID: 1,
-                    MerchantOrderID: 1234567,
-                    MerchantFulfillmentID: 1234567,
-                    FulfillmentDate: '2002-05-01T15:36:33-08:00',
-                    CarrierCode: 'UPS',
-                    ShippingMethod: 'Second Day',
-                    ShipperTrackingNumber: ['1234567890', '1234567899']
+                    OrderFulfillment: {
+                      MerchantOrderID: 1234567,
+                      MerchantFulfillmentID: 1234567,
+                      FulfillmentDate: '2002-05-01T15:36:33-08:00',
+                      FulfillmentData: {
+                        CarrierCode: 'UPS',
+                        ShippingMethod: 'Second Day',
+                        ShipperTrackingNumber: ['1234567890', '1234567899']
+                      }
+                    }
                 });
-                const xml = '<?xml version="1.0" encoding="iso-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber><ShipperTrackingNumber>1234567899</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>'
+                const xml = '<?xml version="1.0" encoding="ISO-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber><ShipperTrackingNumber>1234567899</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>'
+
+                expect(FeedContent).to.equal(xml);
+            });
+
+            it('should handle a multiple fulfillments', () => {
+                const FeedContent = Feeds._POST_ORDER_FULFILLMENT_DATA_([{
+                    MessageID: 1,
+                    OrderFulfillment: {
+                      MerchantOrderID: 1234567,
+                      MerchantFulfillmentID: 1234567,
+                      FulfillmentDate: '2002-05-01T15:36:33-08:00',
+                      FulfillmentData: {
+                        CarrierCode: 'UPS',
+                        ShippingMethod: 'Second Day',
+                        ShipperTrackingNumber: '1234567890'
+                      }
+                    }
+                }, {
+                    MessageID: 2,
+                    OrderFulfillment: {
+                      MerchantOrderID: 9876543,
+                      MerchantFulfillmentID: 9876543,
+                      FulfillmentDate: '2002-05-01T16:30:33-08:00',
+                      FulfillmentData: {
+                        CarrierCode: 'DHL',
+                        ShippingMethod: 'Second Day',
+                        ShipperTrackingNumber: '1234567890'
+                      }
+                    }
+                }]);
+                const xml = '<?xml version="1.0" encoding="ISO-8859-1"?><AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"><Header><DocumentVersion>1.01</DocumentVersion></Header><MessageType>OrderFulfillment</MessageType><Message><MessageID>1</MessageID><OrderFulfillment><MerchantOrderID>1234567</MerchantOrderID><MerchantFulfillmentID>1234567</MerchantFulfillmentID><FulfillmentDate>2002-05-01T15:36:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>UPS</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message><Message><MessageID>2</MessageID><OrderFulfillment><MerchantOrderID>9876543</MerchantOrderID><MerchantFulfillmentID>9876543</MerchantFulfillmentID><FulfillmentDate>2002-05-01T16:30:33-08:00</FulfillmentDate><FulfillmentData><CarrierCode>DHL</CarrierCode><ShippingMethod>Second Day</ShippingMethod><ShipperTrackingNumber>1234567890</ShipperTrackingNumber></FulfillmentData></OrderFulfillment></Message></AmazonEnvelope>';
 
                 expect(FeedContent).to.equal(xml);
             });
         });
     });
 });
-
-
